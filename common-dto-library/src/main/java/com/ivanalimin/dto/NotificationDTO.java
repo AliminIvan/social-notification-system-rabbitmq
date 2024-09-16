07ddf6c0e5e1af77ece24ae38ed6931e91c79618
Ivan Alimin <aliminivan1990@gmail.com>
2024-09-17 00:19:46 +0300

    Practical task - RabbitMQ v 1.0.2

---
+++ common-dto-library/src/main/java/com/ivanalimin/dto/NotificationDTO.java
@@ -0,0 +1,65 @@
+package com.ivanalimin.dto;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class NotificationDTO {
+
+    private String recipient;
+    private String message;
+    private LocalDateTime sentAt;
+
+    public NotificationDTO() {
+    }
+
+    public NotificationDTO(String recipient, String message) {
+        this.recipient = recipient;
+        this.message = message;
+        this.sentAt = LocalDateTime.now();
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getSentAt() {
+        return sentAt;
+    }
+
+    public void setSentAt(LocalDateTime sentAt) {
+        this.sentAt = sentAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof NotificationDTO that)) return false;
+        return Objects.equals(recipient, that.recipient) && Objects.equals(message, that.message) && Objects.equals(sentAt, that.sentAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(recipient, message, sentAt);
+    }
+
+    @Override
+    public String toString() {
+        return "NotificationDTO{" +
+                "recipient='" + recipient + '\'' +
+                ", message='" + message + '\'' +
+                ", sentAt=" + sentAt +
+                '}';
+    }
+}
--- subscriptions/src/main/java/com/ivanalimin/subscriptions/service/SubscriptionService.java
+++ subscriptions/src/main/java/com/ivanalimin/subscriptions/service/SubscriptionService.java
@@ -1,11 +1,13 @@
 package com.ivanalimin.subscriptions.service;
 
+import com.ivanalimin.dto.NotificationDTO;
 import com.ivanalimin.dto.PublicationDTO;
 import com.ivanalimin.subscriptions.model.Subscription;
 import com.ivanalimin.subscriptions.repository.SubscriptionRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,6 +18,7 @@ import java.util.List;
 public class SubscriptionService {
 
     private final SubscriptionRepository repository;
+    private final RabbitTemplate rabbitTemplate;
 
     public Subscription subscribe(Subscription subscription) {
         return repository.save(subscription);
@@ -39,6 +42,8 @@ public class SubscriptionService {
     }
 
     private void sendNotification(String subscriber, String message) {
+        NotificationDTO notificationDTO = new NotificationDTO(subscriber, message);
         log.info("Sending message to subscriber: {} message: {}", subscriber, message);
+        rabbitTemplate.convertAndSend("notifications_subscribers", notificationDTO);
     }
 }
